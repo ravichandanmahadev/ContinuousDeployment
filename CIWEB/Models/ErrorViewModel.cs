da97c5504465ffdcbbfe9dd2201182c5bbb4f3d7
ravichandanm@gmail.com <ravichandanm@gmail.com>
2018-07-31 22:44:48 +0530

    First

---
+++ CIWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CIWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
